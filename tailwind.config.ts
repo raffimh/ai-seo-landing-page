0dbf070642b2cab1af7bd2d5bead5f8d5d0cd804
Raffi Maulana Hadi <raffimaulana25@gmail.com>
2024-11-16 21:37:49 +0700

    refactor: restructure project by removing unused files and adding new assets and components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,33 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "20px",
+        lg: "80px",
       },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "1200px",
+      },
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    extend: {
+      colors: {},
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
