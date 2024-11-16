f3715799487ae702f1399e9f414fdba26cebbb4b
Raffi Maulana Hadi <raffimaulana25@gmail.com>
2024-11-16 22:28:31 +0700

    fix: update Button component props type from empty object to object

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Button(props: React.PropsWithChildren<{}>) {
+export default function Button(props: React.PropsWithChildren<object>) {
   return (
     <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#48208a] shadow-[0px_0px_12px_#8c45ff]">
       <div className="absolute inset-0">
